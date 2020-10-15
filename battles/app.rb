e6033b3b49649d7b702a17a9985183a16b8a0d68
David Millen <70208036+dm-devtech@users.noreply.github.com>
2020-10-15 15:49:39 +0100

    Adding form for player names, second test passes.  First test does not pass but is redundant.

--- battles/app.rb
+++ battles/app.rb
@@ -3,7 +3,13 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
 get '/' do
-  "Hello Battle!"
+  erb(:index)
+end
+
+post '/names' do
+  @Player1_name = params[:Player1_name]
+  @Player2_name = params[:Player2_name]
+  erb(:play)
 end
 
 end
--- battles/spec/features/testing_infrastructure_spec.rb
+++ battles/spec/features/testing_infrastructure_spec.rb
@@ -1,8 +1,18 @@
-require 'app'
+require '././app.rb'
 
 feature 'Testing infrastructure' do
   scenario 'Can run app and check page content' do
     visit('/')
     expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
+
+feature 'Testing form' do
+  scenario 'entering two players names into one form' do
+    visit('/')
+    fill_in('Player1_name', with: 'Tom')
+    fill_in('Player2_name', with: 'Harry')
+    click_button "Submit"
+    expect(page).to have_text("Welcome Tom and Harry")
   end
 end
